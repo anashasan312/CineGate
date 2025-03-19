0a3929570d850c2364f424638171a37c0951edbb
iv-anas <anas.hasan@instaview.ai>
2025-03-19 16:45:26 +0530

    fix-order

--- pkg/infrastructure/config/config.go
+++ pkg/infrastructure/config/config.go
@@ -5,13 +5,13 @@ type AppConfig struct {
 	Server *Server `mapstructure:"server" validate:"required"`
 }
 
+func LoadDefaultConfig() *AppConfig {
+	return &AppConfig{}
+}
+
 func LoadConfig(configPath, secretPath string) (*AppConfig, error) {
 
 	cfg := LoadDefaultConfig()
 
 	return cfg, nil
-}
-
-func LoadDefaultConfig() *AppConfig {
-	return &AppConfig{}
 }
